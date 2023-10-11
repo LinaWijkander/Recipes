3717e4c7ae07c3554078680d7be2087ee02ee8a7
Adrachn <windandwaterdragon@gmail.com>
2023-10-11 11:21:22 +0200

    routing fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import Smoothies from "./components/pages/smoothies";
 import Plan from "./components/pages/plan";
 import Home from "./components/pages/home";
 
-//<Route path="/" element={<Home />} />
+//<Route index element={<Home />} />
 
 function App() {
   return (
     <>
         <Routes>
-            <Route index element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/smoothies" element={<Smoothies />} />
             <Route path="/plan" element={<Plan />} />
         </Routes>
