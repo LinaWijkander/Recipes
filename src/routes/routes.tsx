8ac9961777eb5d16f1ba4881095d86f77c6ee7c1
LinaW <lina@ustwo.com>
2023-10-19 16:49:15 +0200

    revert of hashrouter, no work

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,11 @@
-import {createHashRouter} from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
 import App from "../App";
 import Home from "../components/pages/home";
 import {dogLoader, recipeLoader} from "../loaders/loaders";
 import Smoothies from "../components/pages/smoothies";
 import {Plan} from "../components/pages/plan";
 
-export const router = createHashRouter(
+export const router = createBrowserRouter(
     [{
         path: "/",
         element: <App/>,
