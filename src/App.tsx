fea916e420759258d26dcb7f9fbb7124f120bb7d
Adrachn <windandwaterdragon@gmail.com>
2023-10-19 14:46:44 +0200

    stupid

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 // can use react query instead of this new router
-
 import { Outlet} from "react-router-dom";
 import './assets/variables.css'
 import {GlobalStyle, PageContainer} from "./components/styles/genericContainers";
-import {Navbar} from "./components/organisms/navbar";
+import {Navbar as BestNavbar} from "./components/organisms/navbar";
 
 
 function App() {
     return (
     <PageContainer>
         <GlobalStyle />
-       <Navbar/>
+       <BestNavbar/>
         <Outlet/>
     </PageContainer>
   );
