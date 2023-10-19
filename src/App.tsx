e0d84fd54795d7d18e7827e986ea341369553e9f
Adrachn <windandwaterdragon@gmail.com>
2023-10-19 14:23:07 +0200

    more eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 // can use react query instead of this new router
 
 import { Outlet} from "react-router-dom";
-import {Navbar} from "./components/organisms/navbar";
 import './assets/variables.css'
 import {GlobalStyle, PageContainer} from "./components/styles/genericContainers";
+import {Navbar} from "./components/organisms/navbar/";
 
 function App() {
     return (
