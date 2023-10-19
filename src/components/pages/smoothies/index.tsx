9f99669f88b44e84dbc801e12ab6e4695cd8cf07
Adrachn <windandwaterdragon@gmail.com>
2023-10-19 12:08:44 +0200

    any type error for typescript

--- src/components/pages/smoothies/index.tsx
+++ src/components/pages/smoothies/index.tsx
@@ -2,19 +2,32 @@ import {ContentContainer} from "../../styles/genericContainers";
 import {useLoaderData} from "react-router-dom";
 import RouterLink from "../../atoms/routerLink";
 import RecipeCard from "../../molecules/recipeCard";
+import { LoaderFunction } from 'react-router-dom';
 
- 
-export const recipeLoader = async () => {
+
+export type LoaderData<TLoaderFn extends LoaderFunction> = Awaited<ReturnType<TLoaderFn>> extends Response | infer D
+    ? D
+    : never;
+
+export const recipeLoader = (async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/photos");
     return response.json();
+}) satisfies LoaderFunction; 
+
+
+
+type Recipe = {
+    title: string
+    id: number
+    url: string
 }
 
 const Smoothies = () => {
-    const recipes = useLoaderData() as any;
+    const recipes = useLoaderData() as LoaderData<typeof recipeLoader>;
     
     return (
         <ContentContainer>
-            {recipes.map((recipes : any) => (
+            {recipes.map((recipes : Recipe)  => (
                 <RouterLink to={"/"} key={recipes.id}>
                 <RecipeCard title={recipes.title} image={recipes.url}/>
                 </RouterLink>
