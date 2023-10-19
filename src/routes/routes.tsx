9218487ec08735e4d3774ff144931f8ecb17bd83
Adrachn <windandwaterdragon@gmail.com>
2023-10-19 15:43:51 +0200

    potential blank github pages fix

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,11 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createHashRouter} from "react-router-dom";
 import App from "../App";
 import Home from "../components/pages/home";
 import {dogLoader, recipeLoader} from "../loaders/loaders";
 import Smoothies from "../components/pages/smoothies";
 import {Plan} from "../components/pages/plan";
 
-export const router = createBrowserRouter(
+export const router = createHashRouter(
     [{
         path: "/",
         element: <App/>,
